16bba598d1c320002e8d12bf93958a98c22b4e14
unknown <firasmattoussy@gmail.com>
2019-03-25 00:29:12 +0100

    hosts(linking redux to react)

--- ahla-bik/src/components/hostsPage/mainSectionItem.js
+++ ahla-bik/src/components/hostsPage/mainSectionItem.js
@@ -1,75 +1,9 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
 import HostCard from './hostCard'
 import SideSearch from './sideSearch';
 
-const hoststab=[
-    {
-        _id: 2020,
-        title: "Arabic coffee",
-        host: "Asma FAKHFAKH",
-        activity: "Drink",
-        destination: "Chatt Mariem - Sousse",
-        price: 4,
-        rating: 7,
-        description: "Join the family on a cup of arabic coffee with home-made traditional sweets. We will be chatting about life and cats",
-        imgsrc:'https://static.boredpanda.com/blog/wp-content/uploads/2017/01/two-faced-cat-yana-28.jpg',
-    },
-    {
-        _id: 3030,
-        title: "Lablebi",
-        host: "Houssem Trabelsi",
-        activity: "Food",
-        destination: "Sahloul - Sousse",
-        price: 3,
-        rating: 5,
-        description: "Lablebi is a famous tunisian food made with chickpeas. Warning: be ready to spice it up",
-        imgsrc:'https://steemitimages.com/DQmRpnB8guFd4VkcZgLhtFMbjxqDQeqxzb5r4cYREin64CH/18645915_148001462408917_5174778861115670528_n.jpg',
-    },
-    {
-        _id: 4040,
-        title: "Spéléologie",
-        host: "Rabii Ben Brahim",
-        activity: "Sport",
-        destination: "Ain Dhab - Jendouba",
-        price: 10,
-        rating: 8,
-        description: "Come and discover the amazing cave of Ain Dhab",
-        imgsrc:'https://www.intrepides-jura.com/1409-large_default/initiation-speleo-aquatique.jpg',
-    },
-    {
-        _id: 3030,
-        title: "Lablebi",
-        host: "Houssem Trabelsi",
-        activity: "Food",
-        destination: "Sahloul - Sousse",
-        price: 3,
-        rating: 5,
-        description: "Lablebi is a famous tunisian food made with chickpeas. Warning: be ready to spice it up",
-        imgsrc:'https://steemitimages.com/DQmRpnB8guFd4VkcZgLhtFMbjxqDQeqxzb5r4cYREin64CH/18645915_148001462408917_5174778861115670528_n.jpg',
-    },
-    {
-        _id: 4040,
-        title: "Spéléologie",
-        host: "Rabii Ben Brahim",
-        activity: "Sport",
-        destination: "Ain Dhab - Jendouba",
-        price: 10,
-        rating: 8,
-        description: "Come and discover the amazing cave of Ain Dhab",
-        imgsrc:'https://www.intrepides-jura.com/1409-large_default/initiation-speleo-aquatique.jpg',
-    },
-    {
-        _id: 2020,
-        title: "Arabic coffee",
-        host: "Asma FAKHFAKH",
-        activity: "Drink",
-        destination: "Chatt Mariem - Sousse",
-        price: 4,
-        rating: 7,
-        description: "Join the family on a cup of arabic coffee with home-made traditional sweets. We will be chatting about life and cats",
-        imgsrc:'https://static.boredpanda.com/blog/wp-content/uploads/2017/01/two-faced-cat-yana-28.jpg',
-    }
-]
 
 class MainSection extends Component {
     constructor(props) {
@@ -86,7 +20,7 @@ class MainSection extends Component {
                 <div className="row">
 
                 {
-                    hoststab.map((el,index)=>
+                    this.props.posts.map((el,index)=>
                         <div className="col-sm col-md-6 col-lg-4 ftco-animate fadeInUp ftco-animated">
                             <HostCard key={index} item={el}/>
                         </div>)
@@ -116,5 +50,9 @@ class MainSection extends Component {
         );
     }
 }
- 
-export default MainSection;
+const mapStateToProps=(state)=> {
+    return {posts:state.hostsReducer}
+    
+  }
+  
+export default connect(mapStateToProps)(MainSection)  
--- ahla-bik/src/reducers/index.js
+++ ahla-bik/src/reducers/index.js
@@ -2,5 +2,6 @@ import {combineReducers} from 'redux'
 import reducerguest from './reducerguest'
 import experienceCommentReducer from './experiencesCommentsReducer'
 import experiencesArticlesReducer from './experiencesArticlesReducer'
+import hostsReducer from './hostsreducer'
 
-export default combineReducers({experienceCommentReducer,experiencesArticlesReducer})
+export default combineReducers({experienceCommentReducer,experiencesArticlesReducer,hostsReducer})
